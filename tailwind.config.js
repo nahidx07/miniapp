2dc90885cb95263203b315d58b35d56b2af53e77
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-01-17 17:14:18 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkBg: "#0b0d10",
+        darkCard: "#161b22",
+        primaryRed: "#e02424"
+      }
+    },
+  },
+  plugins: [],
+}
